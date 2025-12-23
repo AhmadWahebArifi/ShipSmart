a160ab594394e42f1f827fd114b2a5da2e5afb1e
= <ahmadwahebarifi@gmail.com>
2025-12-23 20:12:30 +0430

    AppLoader for logging out of system created !

--- Frontend/src/components/AppLoader.jsx
+++ Frontend/src/components/AppLoader.jsx
@@ -3,11 +3,11 @@ import { useLoader } from "../context/LoaderContext";
 import Loader from "./Loader";
 
 const AppLoader = () => {
-  const { showLoader, loaderText } = useLoader();
+  const { showLoader, loaderText, loaderColor } = useLoader();
 
   if (!showLoader) return null;
 
-  return <Loader showText={true} customText={loaderText} size="medium" />;
+  return <Loader showText={true} customText={loaderText} size="medium" color={loaderColor} />;
 };
 
 export default AppLoader;
